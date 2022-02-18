a52b7a7388e7c73fc419e846fefdc30356530eb5
郁帅奇 <shuaiqi@kaixingame.cn>
2022-02-18 11:37:29 +0800

    更改config启动类

--- ali-cloud-config4001/src/main/java/com/genius/cloud/Config4001Application.java
+++ ali-cloud-config4001/src/main/java/com/genius/cloud/Config4001Application.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ConfigMain4001 {
+public class Config4001Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigMain4001.class, args);
+        SpringApplication.run(Config4001Application.class, args);
     }
 
 }
