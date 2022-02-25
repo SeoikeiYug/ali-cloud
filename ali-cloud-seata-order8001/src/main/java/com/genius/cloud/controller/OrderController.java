492c084f29c03fab5351d2ce5aab147eba591e51
郁帅奇 <shuaiqi@kaixingame.cn>
2022-02-25 15:03:30 +0800

    更改文件路径

--- ali-cloud-seata-order8001/src/main/java/com/genius/cloud/controller/OrderController.java
+++ ali-cloud-seata-order8001/src/main/java/com/genius/cloud/controller/OrderController.java
@@ -6,12 +6,14 @@ import com.genius.cloud.service.OrderService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 @Api("订单")
-@RestController("/order")
+@RestController
+@RequestMapping("/order")
 public class OrderController {
 
     @Resource
