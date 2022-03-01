46513a6bab96d55a4aed0f07401ad3154cd28ae1
郁帅奇 <shuaiqi@kaixingame.cn>
2022-03-01 20:10:42 +0800

    rocketmq 广播模式

---
+++ ali-cloud-seata-order8001/src/main/java/com/genius/cloud/config/mq/ConsumerBroadcasting.java
@@ -0,0 +1,42 @@
+package com.genius.cloud.config.mq;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
+
+import java.util.List;
+
+public class ConsumerBroadcasting {
+
+    public static void main(String[] args) throws Exception {
+        // 1.创建消费者Consumer，制定消费者组名
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("genius_producer_broadcasting_group");
+        // 2.指定Nameserver地址
+        consumer.setNamesrvAddr("13.213.65.160:9876");
+        // 默认均衡轮询消费模式 改为广播模式
+        consumer.setMessageModel(MessageModel.BROADCASTING);
+
+        // 3.订阅主题Topic和Tag
+        consumer.subscribe("Topic_broadcasting_genius", "*");
+
+        // 4.设置回调函数，处理消息
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            // 接受消息内容
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                for (MessageExt msg : msgs) {
+                    System.out.println("A----consumeThread=" + Thread.currentThread().getName() + "," + new String(msg.getBody()));
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+        // 5.启动消费者consumer
+        consumer.start();
+
+        System.out.println("消费者启动");
+    }
+}
+
